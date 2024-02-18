6b38d224520d4ebe0f457128436fff1d1667ab04
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2024-02-18 23:41:20 +0300

    public env path

--- src/components/downloadFileBtn/DownloadFileBtn.jsx
+++ src/components/downloadFileBtn/DownloadFileBtn.jsx
@@ -6,6 +6,7 @@ import styled from '@emotion/styled';
 import { slideInBlurredLeft } from '../../theme/slideInBlurredLeft';
 
 
+
 const MyIconBox = styled(Box)(({rotate}) => ({
   display:'flex',
   justifyContent:'end',
